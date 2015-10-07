ddbc978fc826a6c5fd77f9e07ebb022367fef433
Brett Rogerson <brettmrogerson@gmail.com>
2015-10-07 14:24:04 -0400

    Create index.js

---
+++ index.js
@@ -0,0 +1,23 @@
+module.exports = {
+  env: {
+    es6: true
+  },
+  rules: {
+    'arrow-parens': [2, 'always'],
+    'arrow-spacing': [2, { 'before': false, 'after': false }],
+    'constructor-super': 2,
+    'generator-star-spacing': [2, {'before': true, 'after': false}],
+    'no-class-assign': 2,
+    'no-const-assign': 2,
+    'no-dupe-class-members': 2,
+    'no-this-before-super': 2,
+    'no-var': 2,
+    'object-shorthand': [2, 'always'],
+    'prefer-arrow-callback': 2,
+    'prefer-const': 2,
+    'prefer-spread': 2,
+    'prefer-reflect': 2,
+    'prefer-template': 2,
+    'require-yield': 2,
+  }
+};
