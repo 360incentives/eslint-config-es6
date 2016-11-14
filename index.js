84c5cab3ed6351797abc3475a785e2e02274fda8
darrenmce <dmcelligott@360incentives.com>
2016-11-14 10:30:15 -0500

    include all the rules except `no-restricted-imports`

--- index.js
+++ index.js
@@ -3,21 +3,35 @@ module.exports = {
     es6: true
   },
   rules: {
+    'arrow-body-style': 'off',
     'arrow-parens': ['error', 'always'],
     'arrow-spacing': ['error', { before: true, after: true }],
     'constructor-super': 'error',
     'generator-star-spacing': ['error', { before: true, after: false }],
     'no-class-assign': 'error',
+    'no-confusing-arrow': 'off',
     'no-const-assign': 'error',
     'no-dupe-class-members': 'error',
+    'no-duplicate-imports': 'error',
+    'no-new-symbol': 'error',
     'no-this-before-super': 'error',
+    'no-useless-computed-key': 'error',
+    'no-useless-constructor': 'off',
+    'no-useless-rename': 'error',
     'no-var': 'error',
     'object-shorthand': ['error', 'always'],
     'prefer-arrow-callback': 'error',
     'prefer-const': 'error',
+    'prefer-numeric-literals': 'warn',
+    'prefer-rest-params': 'error',
     'prefer-spread': 'error',
     'prefer-template': 'error',
     'require-yield': 'error',
-    'strict': ['error', 'global']
+    'rest-spread-spacing': ['error', 'never'],
+    'sort-imports': 'off',
+    'symbol-description': 'error',
+    'strict': ['error', 'global'],
+    'template-curly-spacing': ['error', 'never'],
+    'yield-star-spacing': ['error', { before: false, after: true }]
   }
 };
