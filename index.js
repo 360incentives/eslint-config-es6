f61f16c5ff9f9fc8e58e76d65878369540e5362a
darrenmce <dmcelligott@360incentives.com>
2016-11-14 10:28:04 -0500

    fix style of index.js

--- index.js
+++ index.js
@@ -3,21 +3,21 @@ module.exports = {
     es6: true
   },
   rules: {
-    'arrow-parens': ["error", 'always'],
-    'arrow-spacing': ["error", { 'before': true, 'after': true }],
-    'constructor-super': "error",
-    'generator-star-spacing': ["error", {'before': true, 'after': false}],
-    'no-class-assign': "error",
-    'no-const-assign': "error",
-    'no-dupe-class-members': "error",
-    'no-this-before-super': "error",
-    'no-var': "error",
-    'object-shorthand': ["error", 'always'],
-    'prefer-arrow-callback': "error",
-    'prefer-const': "error",
-    'prefer-spread': "error",
-    'prefer-template': "error",
-    'require-yield': "error",
-    'strict': ["error", 'global']
+    'arrow-parens': ['error', 'always'],
+    'arrow-spacing': ['error', { before: true, after: true }],
+    'constructor-super': 'error',
+    'generator-star-spacing': ['error', { before: true, after: false }],
+    'no-class-assign': 'error',
+    'no-const-assign': 'error',
+    'no-dupe-class-members': 'error',
+    'no-this-before-super': 'error',
+    'no-var': 'error',
+    'object-shorthand': ['error', 'always'],
+    'prefer-arrow-callback': 'error',
+    'prefer-const': 'error',
+    'prefer-spread': 'error',
+    'prefer-template': 'error',
+    'require-yield': 'error',
+    'strict': ['error', 'global']
   }
 };
